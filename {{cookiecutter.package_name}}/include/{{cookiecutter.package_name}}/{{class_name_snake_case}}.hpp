0dcc1b52f81bacb9ffca9a9344c68ccdf0b7cab7
lgulich <lgulich@ethz.ch>
2019-10-09 09:38:09 +0200

    initial commit

---
+++ {{cookiecutter.package_name}}/include/{{cookiecutter.package_name}}/{{class_name_snake_case}}.hpp
@@ -0,0 +1,16 @@
+#ifndef {{cookiecutter.class_name_all_capital}}_H
+#define {{cookiecutter.class_name_all_capital}}_H
+
+namespace {{cookiecutter.package_name}}
+
+class {{cookiecutter.class_name_camel_case}} {
+
+public:
+  {{cookiecutter.class_name_camel_case}}();
+  virtual ~{{cookiecutter.class_name_camel_case}}();
+
+
+private:
+};
+
+#endif
---
+++ {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/{{cookiecutter.class_name_snake_case}}.cpp
@@ -0,0 +1,9 @@
+#include <{{cookiecutter.package_name}}/{{cookiecutter.class_name_snake_case}}.hpp>
+
+namespace {{cookiecutter.package_name}} {
+
+{{cookiecutter.class_name_camel_case}}::{{cookiecutter.class_name_camel_case}}() {}
+
+{{cookiecutter.class_name_camel_case}}::~{{cookiecutter.class_name_camel_case}}() {}
+
+}
