f633fae5396af306394df503723ddffff8969fb4
lgulich <lgulich@ethz.ch>
2019-10-22 22:25:14 +0200

    Update and rename {{cookiecutter.class_name_snake_case}}.cpp to {{cookiecutter.class_name_snake_case}}.cc

---
+++ {{cookiecutter.package_name}}/src/{{cookiecutter.class_name_snake_case}}.cc
@@ -0,0 +1,5 @@
+#include "{{cookiecutter.package_name}}/{{cookiecutter.class_name_snake_case}}.h"
+
+namespace {{cookiecutter.package_name}} {
+
+}
--- {{cookiecutter.package_name}}/src/{{cookiecutter.class_name_snake_case}}.cpp
+++
@@ -1,9 +0,0 @@
-#include <{{cookiecutter.package_name}}/{{cookiecutter.class_name_snake_case}}.hpp>
-
-namespace {{cookiecutter.package_name}} {
-
-{{cookiecutter.class_name_camel_case}}::{{cookiecutter.class_name_camel_case}}() {}
-
-{{cookiecutter.class_name_camel_case}}::~{{cookiecutter.class_name_camel_case}}() {}
-
-}
