9bb8cb6704f96db6e25425b8f428daf2e20515b1
lgulich <lgulich@ethz.ch>
2019-10-22 22:24:21 +0200

    Update and rename {{cookiecutter.class_name_snake_case}}.hpp to {{cookiecutter.class_name_snake_case}}.h

---
+++ {{cookiecutter.package_name}}/include/{{cookiecutter.package_name}}/{{cookiecutter.class_name_snake_case}}.h
@@ -0,0 +1,16 @@
+#ifndef {{cookiecutter.package_name_all_capital}}{{cookiecutter.class_name_all_capital}}_H_
+#define {{cookiecutter.package_name_all_capital}}{{cookiecutter.class_name_all_capital}}_H_
+
+namespace {{cookiecutter.package_name}} {
+
+class {{cookiecutter.class_name_camel_case}} {
+
+public:
+  {{cookiecutter.class_name_camel_case}}() = default;
+
+private:
+};
+
+} // namespace {{cookiecutter.package_name}}
+
+#endif // {{cookiecutter.package_name_all_capital}}{{cookiecutter.class_name_all_capital}}_H_
