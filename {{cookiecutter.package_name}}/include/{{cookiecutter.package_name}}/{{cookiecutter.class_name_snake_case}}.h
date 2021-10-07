8cf1aaf16ad836caa4bf4d94896060fb3bd9ae4b
lgulich <lgulich@ethz.ch>
2021-10-07 17:50:28 +0200

    Adapt to new arc standards

--- {{cookiecutter.package_name}}/include/{{cookiecutter.package_name}}/{{cookiecutter.class_name_snake_case}}.h
+++ {{cookiecutter.package_name}}/include/{{cookiecutter.package_name}}/{{cookiecutter.class_name_snake_case}}.h
@@ -6,11 +6,10 @@ namespace {{cookiecutter.package_name}} {
 class {{cookiecutter.class_name_camel_case}} {
 
 public:
-  {{cookiecutter.class_name_camel_case}}() = default;
 
 private:
 };
 
 } // namespace {{cookiecutter.package_name}}
 
-#endif // {{cookiecutter.package_name_all_capital}}{{cookiecutter.class_name_all_capital}}_H_
+#endif // {{cookiecutter.package_name_all_capital}}_{{cookiecutter.class_name_all_capital}}_H_
--- {{cookiecutter.package_name}}/test/test_{{cookiecutter.package_name}}.cc
+++ {{cookiecutter.package_name}}/test/test_{{cookiecutter.package_name}}.cc
@@ -1,22 +1,13 @@
-#include <sevencpp_core/test/testing_entrypoint.h>
-
 #include "{{cookiecutter.package_name}}/{{cookiecutter.class_name_snake_case}}.h"
+
+#include <arc_cpp/test/testing_entrypoint.h>
 
 namespace {{cookiecutter.package_name}} {
 
-class {{cookiecutter.class_name_camel_case}}Test : public ::testing::Test {
- protected:
-  {{cookiecutter.class_name_camel_case}}Test() = default;
+TEST({{cookiecutter.class_name_camel_case}}Test, ExampleTestName) {
 
-  static constexpr double kNullConstant = 0;
-};
-
-TEST_F(
-    {{cookiecutter.class_name_camel_case}}Test, ExampleTestName) {
-
-  CHECK_EQ(0, kNullConstant);
 }
 
 } // namespace {{cookiecutter.package_name}}
 
-SEVENCPP_UNITTEST_ENTRYPOINT
+UNITTEST_ENTRYPOINT
